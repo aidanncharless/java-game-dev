d3953a9c1abda31831aa712db64b2cd678244078
k4i4 <aidancharlesxoxo@gmail.com>
2026-09-17 17:34:33 -0400

    Create ExperienceCalculator.java

---
+++ 01-java-fundamentals/ExperienceCalculator.java
@@ -0,0 +1,11 @@
+public class ExperienceCalculator {
+    // EXERCISE 04 - XP SYSTEM
+
+    public class ExperienceCalculator {
+
+        public static void main(String[] args) {
+
+            
+        }
+    }
+}
