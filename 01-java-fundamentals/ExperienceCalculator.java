e788e38beb8bf2739116aa9a113c2c4f6d4ebad3
k4i4 <aidancharlesxoxo@gmail.com>
2026-09-17 17:44:36 -0400

    Update ExperienceCalculator.java

--- 01-java-fundamentals/ExperienceCalculator.java
+++ 01-java-fundamentals/ExperienceCalculator.java
@@ -1,11 +1,20 @@
+// EXERCISE 04 - XP SYSTEM
+
 public class ExperienceCalculator {
-    // EXERCISE 04 - XP SYSTEM
 
-    public class ExperienceCalculator {
+    public static void main(String[] args) {
 
-        public static void main(String[] args) {
+        int playerXP = 350;
+        int questXP = 175;
+        int enemyXP = 75;
 
-            
-        }
+        System.out.println("Starting XP: +" + playerXP);
+        System.out.println("Quest Reward: +" + questXP + " XP");
+        System.out.println("Enemy Reward: +" + enemyXP + " XP");
+
+        int totalXP = playerXP + questXP + enemyXP;
+
+        System.out.println("Total XP: " + totalXP);
+
     }
 }
