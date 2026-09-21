05e07698c913aea60c1561240a3b88df7a4e82e3
k4i4 <aidancharlesxoxo@gmail.com>
2026-09-21 12:22:18 -0400

    Update RPGGame.java
    rpg lab first part

---
+++ rpg-system/RPGGame.java
@@ -0,0 +1,39 @@
+package rpg-system;
+
+public class RPGGame {
+    public static void main(String[] args) {
+
+        String playerName;
+        String characterClass;
+
+        String level;
+        String xp;
+        
+        int health;
+        int maxHealth;
+
+        int mana;
+        int maxMana;
+
+        int attackDmg;
+        int defense;
+
+        int gold;
+
+        double criticalChance;
+        double movementSpeed;
+
+        boolean hasKey;
+        boolean isAlive;
+    
+        System.out.println("GAME STARTS");
+        health = 100;
+        mana = 50;
+        gold = 100;
+        xp = 0;
+
+        System.out.println("Player has: \n" + health + "HP\n" + mana + "Mana\n" + gold + "Gold\n" + xp + "XP");
+
+    }
+    
+}
