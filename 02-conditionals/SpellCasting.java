c2c37aa982ced1049215d5845c3cda5ebd429c04
k4i4 <aidancharlesxoxo@gmail.com>
2026-09-22 16:16:28 -0400

    Create SpellCasting.java
    next part of the lab, i must:
    Starting state:
    
    int mana = 50;
    int spellCost = 25;
    boolean hasStaff = true;
    
    Requirements:
    If the player:
    
    has enough mana
    AND
    has a staff
    
    the program should:
    
    Cast Fireball!
    
    and actually subtract the spell cost from the player's mana.
    Otherwise:
    
    Cannot cast Fireball.
    
    Then print:
    
    Remaining Mana: ??

---
+++ 02-conditionals/SpellCasting.java
@@ -0,0 +1,18 @@
+public class SpellCasting {
+    public static void main(String[] args) {
+
+        int mana = 50;
+        int spellCost = 25;
+        boolean hasStaff = true;
+
+        if (mana >= 25 && hasStaff) {
+            System.out.println("Cast Fireball!");
+            mana -= spellCost;
+            System.out.println("Remaining Mana: " + mana);
+
+        } else {
+            System.out.println("Cannot cast Fireball.");
+            System.out.println("Remaining Mana: " + mana);
+        }
+    }
+}
