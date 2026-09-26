73adfeb553d5a3feefc81c18a66891fd93c15f8e
k4i4 <aidancharlesxoxo@gmail.com>
2026-09-26 14:44:20 -0400

    Update TurnCounter.java
    testing new value for the turn count again

--- 03-loops/TurnCounter.java
+++ 03-loops/TurnCounter.java
@@ -2,7 +2,7 @@
 public class TurnCounter {
     
     public static void main(String[] args) {
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= 8; i++) {
             System.out.println("Turn: " + i);
         }
     }
