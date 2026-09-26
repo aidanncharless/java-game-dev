d90b362f7c82ef7042503a0d87dea8af459c1c3d
k4i4 <aidancharlesxoxo@gmail.com>
2026-09-26 14:32:44 -0400

    Update TurnCounter.java
    first attempt at for loop

---
+++ 03-loops/TurnCounter.java
@@ -0,0 +1,9 @@
+
+public class TurnCounter {
+    
+    public static void main(String[] args) {
+        for (int i = 1; int i <= 5; i++;) {
+            System.out.println("Turn: " + i);
+        }
+    }
+}
