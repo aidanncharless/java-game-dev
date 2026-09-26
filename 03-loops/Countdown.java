6f25ba62693a612dc2c54cb8eef92091364cf9ca
k4i4 <aidancharlesxoxo@gmail.com>
2026-09-26 15:01:36 -0400

    Update Countdown.java
    deleted the quotations marks that were stopping the lab from running

--- 03-loops/Countdown.java
+++ 03-loops/Countdown.java
@@ -3,7 +3,7 @@ public class Countdown {
     public static void main(String[] args) {
 
         for (int i = 5; i <= 1; i--) {
-            System.out.println(""i);
+            System.out.println(i);
         }
 
         System.out.println("GO!");
