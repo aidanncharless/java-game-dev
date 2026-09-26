62a56ebdf580f79b148ad45f62d8260a3d72f5ea
k4i4 <aidancharlesxoxo@gmail.com>
2026-09-26 14:33:54 -0400

    Update TurnCounter.java
    i set it up wrong, i initialized i twice, and ";" wasn't required for the change part of the for loop

--- 03-loops/TurnCounter.java
+++ 03-loops/TurnCounter.java
@@ -2,7 +2,7 @@
 public class TurnCounter {
     
     public static void main(String[] args) {
-        for (int i = 1; int i <= 5; i++;) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println("Turn: " + i);
         }
     }
